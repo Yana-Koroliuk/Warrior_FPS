d96522f7b25b1f89fefed8e670c0490e83c5ee4c
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2025-12-09 17:15:14 +0200

    Adding enemies (#11)

--- FPS/Assets/Scripts/Player/PlayerHealth.cs
+++ FPS/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,6 +1,23 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+
 namespace Assets.Scripts.Player.Characteristics
 {
     public class PlayerHealth : HealthSystem
     {
+        [SerializeField] private Volume _volume;
+        public override void Die()
+        {
+            base.Die();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        public override void Damage(float amount)
+        {
+            base.Damage(amount);
+            _volume.GetComponent<Vignette>().active = true;
+        }
     }
 }
